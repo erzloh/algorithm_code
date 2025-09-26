24c14013bb688d8a19577434aabef5cd95030529
Eric Holzer <eric.holzer.lsn@gmail.com>
2025-09-26 15:14:41 +0900

    update

--- tester.py
+++ tester.py
@@ -1,5 +1,8 @@
 from main import MyAI
+# from framework import Alg3D, Board # 本番用
 import random
+from local_driver import Alg3D, Board # ローカル検証用
+
 
 def add_random_move(board):
     while True:
@@ -21,32 +24,31 @@ def main():
     # Create a 4x4x4 cube filled with 0
     board = [[[0 for _ in range(4)] for _ in range(4)] for _ in range(4)]
     ai = MyAI()
-    result = ai.get_move(board, 1, (0, 0))
-    print(result)
-
-    # for i in range(10):
-    #     # if ai.is_terminal(board):
-    #     #     print("Game Overaaa")
-    #     #     break
-    #     random = add_random_move(board)
-    #     board = add_move(random, board, 2)
-    #     # if ai.is_terminal(board):
-    #     #     print("Game Over2")
-    #     #     break
     #     result = ai.get_move(board, 1, [0, 0])
+
+    for i in range(10):
+        # if ai.is_terminal(board):
+        #     print("Game Overaaa")
+        #     break
+        random = add_random_move(board)
+        board = add_move(random, board, 2)
+        # if ai.is_terminal(board):
+        #     print("Game Over2")
+        #     break
+        result = ai.get_move(board, 1, [0, 0])
         
-    #     print(f"enemy: {random}  AI : {result}")  # Expected output:
-    #     board = add_move(result, board, 1)
+        print(f"enemy: {random}  AI : {result}")  # Expected output:
+        board = add_move(result, board, 1)
         
     
-    # print("New board after AI move:", board)
+    print("New board after AI move:", board)
 
 
-    # for z in range(4):
-    #     print(f"Layer {z}:")
-    #     for y in range(4):
-    #         print(board[z][y])
-    #     print()
+    for z in range(4):
+        print(f"Layer {z}:")
+        for y in range(4):
+            print(board[z][y])
+        print()
 
     # print("Board state: ", board)
 if __name__ == "__main__":
