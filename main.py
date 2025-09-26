eb8250d30429d1ce495525efb4587ca7039d1905
Eric Holzer <eric.holzer.lsn@gmail.com>
2025-09-26 15:50:41 +0900

    yd

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class MyAI(Alg3D):
             new_board = self.result(board, action)
             if self.is_terminal(new_board) and self.end_value == 1:
                 return (action[1], action[2])
-            current = self.alpha_beta_minimax(new_board, False, 0, 9, alpha=-math.inf, beta=math.inf)
+            current = self.alpha_beta_minimax(new_board, False, 0, 8, alpha=-math.inf, beta=math.inf)
             # print("Action :", action, "Score :", current, "\n\n")
             if current > best_score:
                 best_score = current
@@ -157,24 +157,24 @@ class MyAI(Alg3D):
 			# Example values : [-1, 1, 0, 2]
             values = [board[x][y][z] for (x,y,z) in line]
 			
-            if values.count(self.player) == 3 and values.count(0) == 1:
-                score += 100
-            elif values.count(self.player) == 2 and values.count(0) == 2:
-                score += 10
+            # if values.count(self.player) == 3 and values.count(0) == 1:
+            #     score += 100
+            # elif values.count(self.player) == 2 and values.count(0) == 2:
+            #     score += 10
 
-            if values.count(enemy) == 3 and values.count(0) == 1:
-                score -= 100
-            elif values.count(enemy) == 2 and values.count(0) == 2:
-                score -= 10
+            # if values.count(enemy) == 3 and values.count(0) == 1:
+            #     score -= 100
+            # elif values.count(enemy) == 2 and values.count(0) == 2:
+            #     score -= 10
 
         # Position Weight
-        for x in range(4):
-            for y in range(4):
-                for z in range(4):
-                    if board[x][y][z] == self.player:
-                        score += self.position_weights[z][x][y]
-                    elif board[x][y][z] == enemy:
-                        score -= self.position_weights[z][x][y]
+        # for x in range(4):
+        #     for y in range(4):
+        #         for z in range(4):
+        #             if board[x][y][z] == self.player:
+        #                 score += self.position_weights[z][x][y]
+        #             elif board[x][y][z] == enemy:
+        #                 score -= self.position_weights[z][x][y]
 
         return score
 
